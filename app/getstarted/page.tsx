4858c4746353d12e4edd8f1ef0223d6a8ab427a7
Wen Qi Koo <s10222922@connect.np.edu.sg>
2025-02-15 15:01:17 +0800

    get started button

--- app/getstarted/page.tsx
+++ app/getstarted/page.tsx
@@ -3,6 +3,7 @@ import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
 import _bgSnow from "../../public/bg-snow.svg";
+import Link from "next/link";
 
 import type { StaticImageData } from "next/image";
 
@@ -25,11 +26,17 @@ export default async function ProtectedPage() {
       style={{ backgroundImage: `url(${bgSnow.src})` }}
     >
 
-      <div className="container flex grow flex-col items-center justify-center gap-20 px-4 py-16">
+      <div className="container flex grow flex-col gap-20 px-4 py-16">
         <h1 className="mt-20 text-center text-3xl font-extrabold tracking-tight text-white">
-          I am a ... addict
+          I am a gooning addict.....
         </h1>
-        {/* Write code under here */}
+        { <Link
+              href="/register"
+              className="w-40 mx-auto rounded-2xl border-b-4 border-green-700 bg-green-600 px-10 py-3 text-center font-bold uppercase transition hover:border-green-600 hover:bg-green-500 md:min-w-[320px]"
+            >
+              Get started
+            </Link>
+            }
       </div>
     </main>
   );
