3038994d1df096c7a94bce575a9652885360f4e0
Wen Qi Koo <s10222922@connect.np.edu.sg>
2025-02-15 21:54:17 +0800

    remove snowbg from aichat

--- app/aichat/page.tsx
+++ app/aichat/page.tsx
@@ -2,12 +2,7 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import { headers } from "next/headers"; // Server-only module
-import _bgSnow from "../../public/bg-snow.svg";
 import ChatWindow from "@/app/aichat/ChatWindow"; // Client Component
-
-import type { StaticImageData } from "next/image";
-
-const bgSnow = _bgSnow as StaticImageData;
 
 export default async function AIChatPage() {
   const supabase = await createClient();
@@ -24,10 +19,7 @@ export default async function AIChatPage() {
   const userAgent = headerList.get("user-agent") || "Unknown";
 
   return (
-    <main
-      className="flex w-full h-screen flex-col items-center bg-[#FBFFE4] dark:bg-[#3D8D7A] text-white"
-      style={{ backgroundImage: `url(${bgSnow.src})` }}
-    >
+    <main className="flex w-full h-screen flex-col items-center bg-[#FBFFE4] dark:bg-[#3D8D7A] text-white">
       <div className="container flex grow flex-col gap-10 px-4 py-16 max-w-2xl">
         <h1 className="text-center text-3xl font-extrabold tracking-tight text-white">
           AI Chat Interface
