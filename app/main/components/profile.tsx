728e088831d17bfbd25743335e7111f21001308f
Mordax <cyrustan77@gmail.com>
2025-02-15 19:44:22 +0800

    Fix: profile

--- app/main/components/profile.tsx
+++ app/main/components/profile.tsx
@@ -3,13 +3,13 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import { Medal, Flame, Star, SmilePlus } from "lucide-react";
 
-const Profile:NextPage = () => {
+const Profile:NextPage = (props) => {
   return (
     <div>
       <ProfileTopBar />
       <div className="flex justify-center gap-3 pt-14">
         <div className="flex w-full max-w-4xl flex-col gap-5 p-5">
-          <ProfileTopSection />
+          <ProfileTopSection email={props.useremail} joindate={props.joindate}/>
           <ProfileStatsSection />
           <ProfileSupportNetwork />
         </div>
@@ -32,10 +32,10 @@ const ProfileTopBar = () => {
   );
 };
 
-const ProfileTopSection = () => {
-  const name = "John Doe";
-  const username = "@recoveringJohn";
-  const joinedAt = "March 2023";
+const ProfileTopSection = (props:any) => {
+  const name = "Koo Wenqi";
+  const username = props.email
+  const joinedAt = props.joindate
   const daysSober = 250;
   const followersCount = 15;
   const followingCount = 8;
@@ -49,23 +49,23 @@ const ProfileTopSection = () => {
       <div className="flex grow flex-col justify-between gap-3">
         <div className="flex flex-col gap-2">
           <div>
-            <h1 className="text-2xl font-bold">{name}</h1>
-            <div className="text-sm text-gray-400">{username}</div>
+            <h1 className="text-2xl font-bold">{username}</h1>
+            {/* <div className="text-sm dark:text-white-400">{username}</div> */}
           </div>
           <div className="flex items-center gap-3">
-            <span className="text-gray-500">{`Joined ${joinedAt}`}</span>
+            <span className="dark:text-white">{`Joined ${joinedAt}`}</span>
           </div>
           <div className="flex items-center gap-3">
-            <span className="text-gray-500">{`${followingCount} Following / ${followersCount} Supporters`}</span>
+            <span className="dark:text-white-500">{`${followingCount} Following / ${followersCount} Supporters`}</span>
           </div>
         </div>
       </div>
-      <Link
+      {/* <Link
         href="/settings/account"
         className="hidden items-center gap-2 self-start rounded-2xl border-b-4 border-blue-500 bg-blue-400 px-5 py-3 font-bold uppercase text-white transition hover:brightness-110 md:flex"
       >
         Edit profile
-      </Link>
+      </Link> */}
     </section>
   );
 };
@@ -83,21 +83,21 @@ const ProfileStatsSection = () => {
           <SmilePlus className="text-orange-500" size={28} />
           <div className="flex flex-col">
             <span className="text-xl font-bold">{daysSober}</span>
-            <span className="text-sm text-gray-400 md:text-base">Days Sober</span>
+            <span className="text-sm text-gray-400 dark:text-white-500 md:text-base">Days Sober</span>
           </div>
         </div>
         <div className="flex items-center gap-3 rounded-2xl border-2 border-gray-200 p-4">
           <Star className="text-yellow-500" size={28} />
           <div className="flex flex-col">
             <span className="text-xl font-bold">{totalXp}</span>
-            <span className="text-sm text-gray-400 md:text-base">Recovery XP</span>
+            <span className="text-sm text-gray-400 dark:text-white-500 md:text-base">Recovery XP</span>
           </div>
         </div>
         <div className="flex items-center gap-3 rounded-2xl border-2 border-gray-200 p-4">
           <Medal className="text-blue-500" size={28} />
           <div className="flex flex-col">
             <span className="text-xl font-bold">{topMilestones}</span>
-            <span className="text-sm text-gray-400 md:text-base">Major Milestones</span>
+            <span className="text-sm text-gray-400 dark:text-white-500 md:text-base">Major Milestones</span>
           </div>
         </div>
       </div>
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -17,15 +17,21 @@ export default async function AIChatPage() {
     data: { user },
   } = await supabase.auth.getUser();
 
+  
   if (!user) {
     return redirect("/sign-in");
   }
+  const useremail=user.email
+  var joinmonth = new Date(user.confirmation_sent_at).getUTCMonth()
+  var joinday = new Date(user.confirmation_sent_at).getDay()
+  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const joinMonthName = monthNames[joinmonth];
 
   return (
     <main
       className="flex w-full h-screen flex-col items-center bg-[#FBFFE4] dark:bg-[#3D8D7A] text-white"
     >
-        <Profile />
+        <Profile useremail={useremail} joindate={joinday+" "+joinMonthName} />
     
     </main>
   );
