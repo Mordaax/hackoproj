9d25ff3fa2b14c95684e1c533c9f786faa5d0a73
Mordax <cyrustan77@gmail.com>
2025-02-15 14:32:05 +0800

    add: background and getstarted

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main className="flex flex-col min-h-screen w-full">
-            <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+            <nav className="w-full flex justify-center border-b-foreground/10 h-16 mb-10">
               <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                 <div className="flex gap-5 items-center font-semibold">
                   <Link href={"/"}>Home</Link>
@@ -51,7 +51,7 @@ export default function RootLayout({
               {children}
             </div>
 
-            <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+            <footer className="w-full flex items-center justify-center mx-auto text-center text-xs gap-8 py-16">
               <p>
                 A {" "}
                 <a
