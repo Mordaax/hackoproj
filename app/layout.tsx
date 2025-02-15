6676e3d19affc30b32888fa6e958ef03aecf88f7
dalberrt <dalbertchea@gmail.com>
2025-02-16 02:17:37 +0800

    fix: home --> homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
             <nav className="w-full flex justify-center border-b-foreground/10 h-16">
               <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                 <div className="flex gap-5 items-center font-semibold">
-                  <Link href={"/"}>Home</Link>
+                  <Link href={"/homepage"}>Home</Link>
                 </div>
                 {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
               </div>
