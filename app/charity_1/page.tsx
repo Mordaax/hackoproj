861ad88bc39c88faa0ac24ac2e2dc2bf95914f4a
Mordax <cyrustan77@gmail.com>
2025-02-16 12:20:43 +0800

    small update

--- app/charity_1/page.tsx
+++ app/charity_1/page.tsx
@@ -1,7 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
-export default async function AIChatPage() {
+export default async function CharityPage() {
   const supabase = await createClient();
   
   const {
